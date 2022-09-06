6845f65114f54ae1f165bedc9541d1bde913d9cf
Vipin-patelME <112804686+Vipin-patelME@users.noreply.github.com>
2022-09-06 17:07:25 +0530

    Update index.js

--- src/components/EvenOddApp/index.js
+++ src/components/EvenOddApp/index.js
@@ -3,31 +3,51 @@ import {Component} from 'react'
 import './index.css'
 
 class EvenOddApp extends Component {
-  state = {count: 100}
+  state = {count: 0, text1: 'Even', text2: 'Odd'}
 
   onInc = () => {
     this.setState(prevState => {
       console.log(prevState)
-      return {count: Math.random() * 100}
+      return {count: prevState.count + Math.random() * 100}
     })
   }
 
   render() {
-    const {count} = this.state
-    return (
-      <div className="bg-Contaimer">
-        <h1 className="">
-          Count <span>0</span>
-        </h1>
-        <p>
-          Count is <span>0</span>
-        </p>
-        <button className="increamentBtn" type="button" onClick={this.onInc}>
-          Increament
-        </button>
-        <p>*Increase by random number between 0 to 100</p>
-      </div>
-    )
+    const {count, text1, text2} = this.state
+    let increamentContainer
+    if (Math.round(count) % 2 === 0) {
+      increamentContainer = (
+        <div>
+          <h1 className="">
+            Count <span>{Math.round(count)}</span>
+          </h1>
+          <p>
+            Count is <span>{text1}</span>
+          </p>
+          <button className="increamentBtn" type="button" onClick={this.onInc}>
+            Increment
+          </button>
+          <p>*Increase by random number between 0 to 100</p>
+        </div>
+      )
+    } else {
+      increamentContainer = (
+        <div>
+          <h1 className="">
+            Count <span>{Math.round(count)}</span>
+          </h1>
+          <p>
+            Count is <span>{text2}</span>
+          </p>
+          <button className="increamentBtn" type="button" onClick={this.onInc}>
+            Increment
+          </button>
+          <p>*Increase by random number between 0 to 100</p>
+        </div>
+      )
+    }
+    return <div className="bg-Contaimer"> {increamentContainer} </div>
   }
 }
 export default EvenOddApp
+
